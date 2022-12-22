a55e2aafc58d5c6310afbc19863d67ed7918bad4
Farhad Hossain <iamforu299@gmail.com>
2022-12-22 22:49:34 +0600

    first commit

---
+++ src/main/java/com/springapi/farhadcodesacpblog/FarhadcodesACpBlogApplication.java
@@ -0,0 +1,13 @@
+package com.springapi.farhadcodesacpblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarhadcodesACpBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarhadcodesACpBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springapi/farhadcodesacpblog/FarhadcodesACpBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.farhadcodesacpblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarhadcodesACpBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
