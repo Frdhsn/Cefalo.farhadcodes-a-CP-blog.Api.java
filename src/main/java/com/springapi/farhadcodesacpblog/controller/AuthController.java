333a620967b13d1cd9ffe6185e951f75552b2ec9
Farhad Hossain <iamforu299@gmail.com>
2022-12-30 05:49:11 +0600

    small bug. added based on #4 #7

--- src/main/java/com/springapi/farhadcodesacpblog/controller/AuthController.java
+++ src/main/java/com/springapi/farhadcodesacpblog/controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.springapi.farhadcodesacpblog.controller;
 
 import com.springapi.farhadcodesacpblog.entity.Users;
+import com.springapi.farhadcodesacpblog.security.AuthenticateManager;
 import com.springapi.farhadcodesacpblog.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
@@ -14,20 +14,25 @@ import java.nio.file.AccessDeniedException;
 public class AuthController {
     @Autowired
     private AuthService authService;
+    @Autowired
+    private AuthenticateManager authenticateManager;
     @PostMapping(path="/users/signup")
     public ResponseEntity<?> signUp(@RequestBody Users users)  {
         Users newUser =authService.signUp(users);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        String token = authenticateManager.authenticate(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(token);
     }
 
     @PostMapping(path="/users/login")
     public ResponseEntity<?> logIn(@RequestBody Users users) {
         Users newUser = null;
+        String token;
         try {
             newUser = authService.Login(users);
+            token = authenticateManager.authenticate(newUser);
         } catch (AccessDeniedException e) {
             throw new RuntimeException(e);
         }
-        return ResponseEntity.status(HttpStatus.OK).body(newUser);
+        return ResponseEntity.status(HttpStatus.OK).body(token);
     }
 }
